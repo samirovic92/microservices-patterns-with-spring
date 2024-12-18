331921465f9bda8d9fead4dcc43be47b386ee24f
selidrissi <s.elidrissi1992@gmail.com>
2024-12-18 23:58:52 +0100

    handle the @CommandExecutionException

--- ProductsService/src/main/java/com/samic/ProductsService/core/errohandling/ProductServiceErrorHandler.java
+++ ProductsService/src/main/java/com/samic/ProductsService/core/errohandling/ProductServiceErrorHandler.java
@@ -1,5 +1,6 @@
 package com.samic.ProductsService.core.errohandling;
 
+import org.axonframework.commandhandling.CommandExecutionException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,12 @@ public class ProductServiceErrorHandler {
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleException(Exception exception) {
+        var errorMessage = new ErrorMessage(LocalDateTime.now(), exception.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {CommandExecutionException.class})
+    public ResponseEntity<Object> handleCommandExecutionException(CommandExecutionException exception) {
         var errorMessage = new ErrorMessage(LocalDateTime.now(), exception.getMessage());
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
